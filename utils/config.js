e6ea5e52b00bc4a93842f8300be6a09c11543849
Tim <tim@192.168.10.13>
2022-11-01 16:30:55 +0200

    fix cors

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const {
   MoviesExplorerdb = 'mongodb://localhost:27017/moviesdb',
-  PORT = 3004,
+  PORT = 3000,
   NODE_ENV,
   JWT_SECRET,
 } = process.env;
